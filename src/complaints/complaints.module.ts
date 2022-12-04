98b6973197b87fa7a9dbfa8b192089b845fce4ac
ricardonghaywi <99095289+ricardonghaywi@users.noreply.github.com>
2022-12-04 23:25:21 +0200

    Update complaints.module.ts

--- src/complaints/complaints.module.ts
+++ src/complaints/complaints.module.ts
@@ -3,11 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ComplaintsService } from './complaints.service';
 import { ComplaintsController } from './complaints.controller';
 import { ComplaintSchema } from './schemas/complaint.schema';
+import { UsersModule } from 'src/users/users.module';
+
 
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'Complaint' , schema: ComplaintSchema}])],
+  imports: [MongooseModule.forFeature([{name: 'Complaint' , schema: ComplaintSchema}]),
+UsersModule],
   providers: [ComplaintsService],
   controllers: [ComplaintsController]
 })
