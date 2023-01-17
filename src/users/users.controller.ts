f217651f82ab3483403569b52af8d8dd3672d98e
ricardonghaywi <99095289+ricardonghaywi@users.noreply.github.com>
2023-01-17 21:57:08 +0200

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller,Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { AuthService } from '../auth/auth.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 
 
@@ -8,7 +10,7 @@ import { AuthService } from '../auth/auth.service';
 export class UsersController {
     constructor(private AuthService: AuthService) {}
 
-
+    @Serialize(UserDto)
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         return this.AuthService.Singup(body.firstName, body.lastName, body.email, body.password, body.isVip, body.isAdmin);
