4158b3d7528be1882dcc8f69305a81139c663975
ricardonghaywi <99095289+ricardonghaywi@users.noreply.github.com>
2023-01-17 21:52:47 +0200

    Update user.dto.ts

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,1 +1,21 @@
+import { Expose } from 'class-transformer';
 
+export class UserDto {
+
+    @Expose()
+    firstName: string;
+
+    @Expose()
+    lastName: string;
+ 
+    @Expose()
+    email: string;
+    
+    @Expose()
+    isVip: boolean;
+     
+    @Expose()
+    isAdmin: boolean;
+
+    
+}
